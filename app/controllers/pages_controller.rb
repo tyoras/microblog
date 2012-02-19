c8c40e751fa79b781e2c5ce86c6d9f7aaddd323d
Yoan <tyoras@gmail.com>
2012-02-20 00:56:22 +0100

    ajout de pages statiques

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,13 @@
 class PagesController < ApplicationController
   def home
+		@titre = "Accueil"
   end
 
   def contact
+  	@titre = "Contactez"
+  end
+
+  def about
+  	@titre = "A propos"
   end
 end
