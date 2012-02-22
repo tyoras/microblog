edd7ee0e00749a899cc07a5a1b0002efefb6da08
Yoan <tyoras@gmail.com>
2012-02-22 18:57:44 +0100

    config prod css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,5 @@ Microblog::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.assets.precompile += %w( blueprint/screen.css blueprint/print.css blueprint/ie.css )
 end
