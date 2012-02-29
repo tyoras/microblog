f1eacc1d5ed806038aa8ae898be553a4211605cc
Yoan <tyoras@gmail.com>
2012-02-29 20:23:12 +0100

    compile css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,6 +17,7 @@ Microblog::Application.configure do
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
   config.assets.initialize_on_precompile = false
+  config.assets.precompile += %w( blueprint/screen.css blueprint/print.css blueprint/ie.css )
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -65,5 +66,4 @@ Microblog::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.assets.precompile += %w( blueprint/screen.css blueprint/print.css blueprint/ie.css )
 end
