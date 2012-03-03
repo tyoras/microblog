eb91e4ee3f8bc50838d846bfb813ad1c7e834c7b
Yoan <tyoras@gmail.com>
2012-03-03 16:29:48 +0100

    changement des tests unitaires

--- push.sh
+++
@@ -1,6 +0,0 @@
-rake assets:clean
-RAILS_ENV=production bundle exec rake assets:precompile
-git add public/assets
-git commit -am "precompile assets"
-git push
-git push heroku
--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -1,30 +1,52 @@
 require 'spec_helper'
 
 describe "Static pages" do
-	let(:base_title) { "Marmotte Blog" }
   subject { page }
   
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1',    text: heading) }
+    it { should have_selector('title', text: titre(page_title)) }
+  end
+
   describe "Home page" do
     before { visit root_path }
-    it {should have_selector("h1", :text => 'Marmotte Blog')}
-    it {should have_selector("title", :text => titre('Accueil'))}
+    let(:heading)    { 'Marmotte Blog' }
+    let(:page_title) { 'Accueil' }
+    it_should_behave_like "all static pages"
   end
 
   describe "Help page" do
     before { visit help_path }
-    it {should have_selector("h1", :text => 'Aide')}
-    it {should have_selector("title", :text => titre('Aide'))}
+    let(:heading)    { 'Aide' }
+    let(:page_title) { 'Aide' }
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     before { visit about_path }
-    it {should have_selector("h1", :text => 'A Propos')}
-    it {should have_selector("title", :text => titre('A Propos'))}
+    let(:heading)    { 'A Propos' }
+    let(:page_title) { 'A Propos' }
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
     before { visit contact_path }
-    it {should have_selector("h1", :text => 'Contact')}
-    it {should have_selector("title", :text => titre('Contact'))}
+    let(:heading)    { 'Contact' }
+    let(:page_title) { 'Contact' }
+    it_should_behave_like "all static pages"
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "A Propos"
+    page.should have_selector 'title', text: titre('A Propos')
+    click_link "Aide"
+    page.should have_selector 'title', text: titre('Aide')
+    click_link "Contact"
+    page.should have_selector 'title', text: titre('Contact')
+    click_link "Accueil"
+    page.should have_selector 'title', text: titre('Accueil')
+    click_link "S'inscrire !"
+    page.should have_selector 'title', text: titre('Inscription')
   end
 end
