e9108cdc500df37a4dd86f2f08d0aea12831a9ec
Yoan <tyoras@gmail.com>
2012-05-13 20:15:49 +0200

    test assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Microblog::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
   config.assets.initialize_on_precompile = false
   config.assets.precompile += [ "blueprint/*.css" ]
 
